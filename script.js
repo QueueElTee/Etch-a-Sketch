8233fafe16b9c7003cb2147eafd431b12329275d
QueueElTee <tijaniqueen17@gmail.com>
2022-08-05 13:10:18 +0100

    Add Functions To Adjust Colors and Size of Grid

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const container = document.querySelector('#container');
-const gridCount = document.querySelector('.grid-count');
+const gridCountText = document.querySelector('.grid-count');
+const slider = document.querySelector('#myRange');
+const colorPicker = document.querySelector('#favcolor');
+const randomColor = document.querySelector('.random-color');
+const eraseSketch = document.querySelector('.erase-sketch');
+
+let gridColor = 'black';
+
 
 let createGrids = (gridValue) => {
     container.style.cssText = `grid-template-columns: repeat(${gridValue}, 1fr)`;
@@ -18,6 +25,51 @@ let colorGrids = (color) => {
     }));
 }
 
+let clearGrids = () => {
+    const grids = document.querySelectorAll('#container div');
+    grids.forEach(grid => container.removeChild(grid));
+}
 
-createGrids(16);
-colorGrids('black');
+
+slider.oninput = function(){
+    let gridCount = this.value;
+    gridCountText.textContent = `${gridCount} x ${gridCount}`;
+    clearGrids();
+    createGrids(gridCount);
+    colorGrids(gridColor);
+}
+
+
+colorPicker.addEventListener('change', (e) => {
+    gridColor = e.target.value;
+    colorGrids(gridColor);
+});
+
+
+let getRandomColor = () => {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+
+    for (var i = 0; i < 6; i++){
+       color += letters[(Math.floor(Math.random() * 16))];
+    }
+
+    return color;
+}
+
+
+randomColor.addEventListener('click', () => {
+    const grids = document.querySelectorAll('#container div');
+    grids.forEach(grid => grid.addEventListener('mouseover', () => grid.style.backgroundColor = getRandomColor()));
+});
+
+
+eraseSketch.addEventListener('click', () => {
+    const grids = document.querySelectorAll('#container div');
+    grids.forEach(grid => grid.style.cssText = 'border: 1px solid black; background-color: white');
+});
+
+
+
+createGrids(10);
+colorGrids(gridColor);
