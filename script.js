6998fdc4f77b2898286b55f180378959ef9ac968
QueueElTee <tijaniqueen17@gmail.com>
2022-08-05 10:27:23 +0100

    Add Grid Size

---
+++ script.js
@@ -0,0 +1,18 @@
+const container = document.querySelector('#container');
+
+let createGrids = (gridValue) =>{
+    container.style.cssText = `grid-template-columns: repeat(${gridValue}, 1fr)`;
+    for(let i = 0; i < gridValue ** 2; i++){
+        const div = document.createElement('div');
+        div.style.cssText = 'border: 1px solid black';
+        container.appendChild(div);
+    }
+}
+
+createGrids(16);
+
+const grids = document.querySelectorAll('#container div');
+grids.forEach(grid => grid.addEventListener('mouseover', () => {
+    grid.style.cssText = `background-color: black`;
+}));
+
