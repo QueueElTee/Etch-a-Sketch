5062ab659c3b9a312fdf6cf2ac95182b5bdb472f
QueueElTee <tijaniqueen17@gmail.com>
2022-08-06 12:32:16 +0100

    Back To Touchstart

--- script.js
+++ script.js
@@ -27,9 +27,7 @@ let colorGrids = (color, event) => {
 
 const checkDeviceType = () => {  
     if(('ontouchstart' in window) ||  (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0)){
-        // colorGrids(gridColor, 'touchstart');
-        // colorGrids(gridColor, 'mouseover');
-        colorGrids(gridColor, 'touchmove');
+        colorGrids(gridColor, 'touchstart');
     }
     else {
         colorGrids(gridColor, 'mouseover');
